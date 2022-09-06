9f3bd95721dd32c5571fc41dd0f3220cc7eb7b6e
Alfredo <fredy_rios0123@outlook.com>
2022-09-06 15:15:01 -0700

    Started with a template for the High Score

--- src/Highscore.js
+++ src/Highscore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 
 class Highscore extends React.Component {
   // constructor(props) {
@@ -7,8 +8,32 @@ class Highscore extends React.Component {
 
   render() {
     return (
-      <>
-      </>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>10</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>12</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>9</td>
+          </tr>
+        </tbody>
+      </Table>
     )
   }
 }
